76cd01aac77ae0ee13d09596c554955d8765c33d
AndrewBiscuit <andrewalford1@outlook.com>
2018-03-24 15:48:31 +0000

    Solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,40 @@
 #include <mbed.h>
+#include "components.h"
 
-DigitalOut green(LED_GREEN);
-void  on(void) { green.write(0); }
-void off(void) { green.write(1); }
+AssignmentBoard board;
 
-Ticker  pit;
+LED green(board.SHIELD_RED_LED);
+
+Ticker pit;
+
 unsigned int period = 100;
-unsigned int duration = 10;
-void centisecond(void) {
-   static unsigned int cs=0;
-   cs = (cs+1) % period ;
 
-   if( cs<duration )  on();
-   else              off();
+unsigned int duration = 10;
+
+void centisecond(void)
+{
+   static unsigned int cs=0;
+   cs = (cs+1) % period;
+
+   if(cs<duration)
+   {
+       green.on();
+   }
+   else
+   {
+     green.off();
+   }
 }
 
-int main(void) {
+int main(void)
+{
+  //Call the function centisecond once every 10000
+  //microseconds. (every 0.01 seconds (1 centisecond)).
   pit.attach_us(centisecond, 10000);
 
-  while(1) {}
+  while(1)
+  {
+    //ground.
+  }
 
 }
